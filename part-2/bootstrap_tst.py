0adada02ce6cdd0d42cc28cdb116def5152c76ea
William West <wwest@linkedin.com>
2018-11-21 14:07:35 -0800

    Complete chapter 21

--- part-2/bootstrap_tst.py
+++ part-2/bootstrap_tst.py
@@ -6,13 +6,27 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
     def tearDown(self):
         pass
+
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount += 1
+
+    def summary(self):
+        return '{} run, 0 failed'.format(self.runCount)
 
 
 class WasRun(TestCase):
@@ -29,6 +43,9 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log += 'tearDown '
 
+    def testBrokenMethod(self):
+        raise Exception
+
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
@@ -36,4 +53,16 @@ class TestCaseTest(TestCase):
         test.run()
         assert('setUp testMethod tearDown ' == test.log)
 
+    def testResult(self):
+        test = WasRun('testMethod')
+        result = test.run()
+        assert('1 run, 0 failed' == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun('testBrokenMethod')
+        result = test.run()
+        assert('1 run, 1 failed' == result.summary())
+
 TestCaseTest('testTemplateMethod').run()
+TestCaseTest('testResult').run()
+# TestCaseTest('testFailedResult').run()
